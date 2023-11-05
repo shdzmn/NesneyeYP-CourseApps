3c1ce069f06378889c29a5b31ee56d428f99e184
suhed <suhed@SUHEDA>
2023-11-05 19:51:27 +0300

    Add project files.

---
+++ W01/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace w01  // maviler kelimeler; keywords
+{
+    internal class Program
+    {
+        //starting or entry point method/function
+        static void Main (string[] args)
+        {
+            int a = 1 + 2; int b = 3 + 4; int c = 5 + 6; // numerical literal
+            Console.WriteLine("hello"); // string literal
+
+        }
+    }
+}
+
+// Tokens;
+// 1 - keywords (maviler: namespace, class gibi)
+// 2 - identifies (veriable definitions: int gibi)
+// 3 - symbols ( [] , {} gibi)
+// 4 - operators ( =,+ gibi)
+// 5 - litearls: Numerical or String
