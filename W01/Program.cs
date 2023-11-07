b0fc8c322b6966d1deb41f413d39e8ee568eb891
SAİDE ŞÜHEDA DİZMAN <suheda.dizman@std.yildiz.edu.tr>
2023-11-07 15:56:59 +0300

    u

--- W01/Program.cs
+++ W01/Program.cs
@@ -10,7 +10,70 @@ namespace w01  // maviler kelimeler; keywords
             int a = 1 + 2; int b = 3 + 4; int c = 5 + 6; // numerical literal
             Console.WriteLine("hello"); // string literal
 
+            // region: progrraming ile ilgili değil, for description 
+            #region Data Types
+
+            #region Value Types / Primitives Types
+            #region Predefined Types 
+            //numerical and boolean types içerir. Aşağıdaki gibi;
+            int a1 = 5;
+            float f1 = 5.5f;
+            double d1 = 6.5;
+            bool b2 = false;
+            char c1 = 'a';
+            #endregion
+            #region User DEfined Types
+            // structs and enum types:
+            //see example 1
+            #endregion
+            #endregion
+            #region Pointer Types - not used so much
+
+            unsafe
+            {
+                int* p1 = &b;
+            }
+
+            #endregion
+            #region Reference Types
+            #region Predifined Types
+            // ÖNEMLİ: string data types reference tipe girer
+            string name = "suheda dizman";
+            object anyData = new MyData();
+
+            #endregion
+            #region User Difined Types
+            //classes and user defined everything
+            //class Program{}=>  user defined reference type
+            //see example 2
+
+            #endregion
+            #endregion
+            #endregion
+
         }
+    }
+
+
+    //Example 1: User Defined Complex Primitive types/value types
+    struct MyData
+    {
+        int p1 = 1;
+        //..
+
+        double d1;
+        public MyData()
+        {
+        }
+    }
+
+    enum DayNames
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Friday,
+        Sunday,
     }
 }
 
@@ -21,4 +84,3 @@ namespace w01  // maviler kelimeler; keywords
 // 4 - operators ( =,+ gibi)
 // 5 - litearls: Numerical or String
 
-//....
